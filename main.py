8356dc70f717e4300d22a23d35dba39101554706
DinoHerlentz <87923606+DinoHerlentz@users.noreply.github.com>
2022-06-24 15:58:14 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from imdb import IMDb
+import time
+
+
+moviesDB = IMDb()
+query = input("Enter movie name : ")
+movies = moviesDB.search_movie(query)
+
+"""
+for movie in movies:
+    title = movie['title']
+    year = movie['year']
+    print(f"{title} - {year}")
+"""
+
+id = movies[0].getID()
+movie = moviesDB.get_movie(id)
+
+title = movie['title']
+year = movie['year']
+rating = movie['rating']
+directors = movie['directors']
+casting = movie['cast']
+
+print(f"Movie Info - {query}")
+print(f"Movie Name : {title} - {year}")
+print(f"Rating : {rating}")
+
+direcStr = " ".join(map(str, directors))
+print(f"Directors : {direcStr}")
+
+actors = ", ".join(map(str, casting))
+print(f"Actors : {actors}")
