f47a3fc0ecd2b895d1bb0cf7b7339db4015bd594
DinoHerlentz <87923606+DinoHerlentz@users.noreply.github.com>
2022-06-25 09:29:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,15 +19,16 @@ movie = moviesDB.get_movie(id)
 title = movie['title']
 year = movie['year']
 rating = movie['rating']
-directors = movie['directors']
+# directors = movie['directors']
 casting = movie['cast']
 
-print(f"Movie Info - {query}")
-print(f"Movie Name : {title} - {year}")
+print(f"Movie Info - {query.title()}")
+print(f"Movie Name : {title}")
+print(f"Movie Year : {year}")
 print(f"Rating : {rating}")
 
-direcStr = " ".join(map(str, directors))
-print(f"Directors : {direcStr}")
+# direcStr = " ".join(map(str, directors))
+# print(f"Directors : {direcStr}")
 
 actors = ", ".join(map(str, casting))
 print(f"Actors : {actors}")
